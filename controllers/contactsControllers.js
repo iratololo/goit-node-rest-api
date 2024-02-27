b0818870f46eece4a0073caded3fefb03f4626bc
iratololo <iratololo1991@gmail.com>
2024-02-27 17:35:44 +0300

    done

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,56 @@
 import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js"
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res, next) => {
+    try {
+    const result = await contactsService.listContacts();
+    res.status(200).json(result);
+    } catch (error) {
+       next(error)
+    }
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res,next) => {
+    try {
+        const result = await contactsService.getContactById(req.params.id);
+        if (!result) {
+           throw HttpError(404, "Not found")
+        }
+    res.status(200).json(result);
+    } catch (error) {
+        next(error)
+    }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+    try {
+        const result = await contactsService.removeContact(req.params.id);
+        if (!result) {
+           throw HttpError(404, "Not found")
+        }
+        res.json(result); 
+    } catch (error) {
+        next(error)
+    }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res, next) => {
+    try {
+        const result = await contactsService.addContact(req.body);
+        res.status(201).json(result);
+    } catch (error) {
+        next(error)
+    }
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+    try {
+        const result = await contactsService.updateContact(req.params.id, req.body);
+        if (!result) {
+           throw HttpError(404, "Not found")
+        }
+        res.status(200).json(result);
+    } catch (error) {
+       next(error) 
+    }
+};
--- helpers/validateBody.js
+++ helpers/validateBody.js
@@ -2,6 +2,7 @@ import HttpError from "./HttpError.js";
 
 const validateBody = (schema) => {
   const func = (req, _, next) => {
+    console.log('validateBody :>> ', req);
     const { error } = schema.validate(req.body);
     if (error) {
       next(HttpError(400, error.message));
